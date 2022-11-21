84f53ec2a845ce42efa8a4d845e5e38124b333c9
liuyang <yang.liu2@northeastern.edu>
2022-11-20 20:46:46 -0500

    add a8

--- src/tuiter/tuits/TuitsList/TuitItem.js
+++ src/tuiter/tuits/TuitsList/TuitItem.js
@@ -7,38 +7,30 @@ import {deleteTuitThunk} from "../../../services/tuits-thunks";
 const TuitItem = ({ tuit }) => {
     const dispatch = useDispatch();
     const deleteTuitHandler = (id) => {
-        // dispatch(deleteTuit(id));
         dispatch(deleteTuitThunk(id));
     };
     return (
-        <>
-            <li className="list-group-item">
-                <div className="row">
-                    <div className="col-auto">
-                        <img alt="user_avatar"
-                             className="float-end rounded-pill" width={50} height={50}
-                             src={`/images/${tuit.image}`}/>
-                    </div>
-                    <div className="col-10">
-                        <span className="fw-bolder">{tuit.userName}</span>
-                        <span>
-              <a href="/">
-                <i className="bi bi-patch-check-fill p-2"></i>
-              </a>
-            </span>
-                        <span className="text-muted">
-              {tuit.handle} · {tuit.time}
-            </span>
-                        <span>
-              <i className="bi bi-x-lg float-end"
-                  onClick={() => deleteTuitHandler(tuit._id)}></i>
-            </span>
-                        <div>{tuit.tuit}</div>
-                        <TuitStats tuit={tuit} />
-                    </div>
+        <li className="list-group-item">
+            <div className="row">
+                <div className="col-auto">
+                    <img className="float-end rounded-circle custom-image" src={`/images/${tuit.image}`} width={50} height={50} alt="avatar"/>
                 </div>
-            </li>
-        </>
+
+                <div className="col-10">
+                    <i className="bi bi-x-lg float-end"
+                       onClick={() => deleteTuitHandler(tuit._id)}/>
+                    <div className="fw-bolder">
+                        {tuit.username} <i className="bi bi-patch-check-fill text-primary px-2"/>
+                        <span className="fw-normal">{tuit.handle} · {tuit.time}</span>
+                    </div>
+
+                    <div className="mb-3 pe-2">{tuit.tuit}</div>
+
+                    <TuitStats tuit={tuit}/>
+                </div>
+
+            </div>
+        </li>
     );
 };
 export default TuitItem;
